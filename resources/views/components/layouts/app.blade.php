5de8b5500e0392a864304bb6e8c4d80743dc1e72
Zyrhea16 <zyrheab@gmail.com>
2025-11-13 14:29:07 +0800

    Part 11 - E-Commerce Project with Laravel 10, Livewire 3, Filament 3 & Tailwind CSS

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,9 +11,11 @@
 </head>
 
 <body class="bg-slate-200 dark:bg-slate-700">
+    @livewire('partials.navbar')
     <main>
         {{ $slot }}
     </main>
+    @livewire('partials.footer')
     @livewireScripts
 </body>
 
