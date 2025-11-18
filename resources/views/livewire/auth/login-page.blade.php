b59a5b32b90e48dd222f639cafb9776662f49d71
Zyrhea16 <zyrheab@gmail.com>
2025-11-18 15:16:08 +0800

    Part 20 - E-Commerce Project with Laravel 10, Livewire 3, Filament 3 & Tailwind CSS

--- resources/views/livewire/auth/login-page.blade.php
+++ resources/views/livewire/auth/login-page.blade.php
@@ -17,6 +17,11 @@
 
           <!-- Form -->
           <form wire:submit.prevent='save'>
+            @if (session('error'))
+            <div class="bg-red-500 text-sm text-white rounded-lg p-4 mb-4" role="alert">
+                <span class="font-bold">{{ session('error') }}</span>
+            </div>
+            @endif
             <div class="grid gap-y-4">
               <!-- Form Group -->
               <div>
