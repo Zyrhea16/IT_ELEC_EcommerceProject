9d88efa7fc489353e6920115298d8090d03b4a06
Zyrhea16 <zyrheab@gmail.com>
2025-11-18 19:29:18 +0800

    Part 25 - E-Commerce Project with Laravel 10, Livewire 3, Filament 3 & Tailwind CSS

--- app/Livewire/MyOrderDetailPage.php
+++ app/Livewire/MyOrderDetailPage.php
@@ -2,12 +2,32 @@
 
 namespace App\Livewire;
 
+use App\Models\OrderItem;
+use App\Models\Order; // Added based on usage in render
+use App\Models\Address; // Added based on usage in render
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Order Detail')]
 class MyOrderDetailPage extends Component
 {
+    public $order_id;
+
+    public function mount($order_id)
+    {
+        $this->order_id = $order_id;
+    }
+
     public function render()
     {
-        return view('livewire.my-order-detail-page');
+        $order_items = OrderItem::with('product')->where('order_id', $this->order_id)->get();
+        $address = Address::where('order_id', $this->order_id)->first();
+        $order = Order::where('id', $this->order_id)->first(); 
+
+        return view('livewire.my-order-detail-page', [
+            'order_items' => $order_items,
+            'address' => $address,
+            'order' => $order
+        ]);
     }
 }
